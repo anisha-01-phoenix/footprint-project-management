c4182153aacb3f9e8322b508770c97526e04c24c
anisha-01-phoenix <anisha.dat01@gmail.com>
2023-12-19 11:56:23 +0530

    CORS added

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const routes = require('./src/routes');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-
 require('dotenv').config();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 app.use('/', routes);
 const PORT = process.env.PORT || 3000;
 connectToDatabase();
