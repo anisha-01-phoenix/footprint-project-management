ce4cb950059a605e940abac2e7ad2d52e2ec6893
Arnab <iamarnabbiswas2001@gmail.com>
2023-12-22 12:15:42 +0530

    Arnab

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const { connectToDatabase } = require('./src/utils/dbConnect');
 const { initDatabase } = require('./dbInitialisation');
 const { query } = require('./db');
 const routes = require('./src/routes');
+const cors=require('cors')
 
 const cookieParser = require('cookie-parser');
 
 const app = express();
 require('dotenv').config();
 
+app.use(cors())
+
 app.use(express.json());
 app.use(cookieParser());
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+
   
 app.use('/', routes);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 connectToDatabase();
 initDatabase()
     .then(() => {
